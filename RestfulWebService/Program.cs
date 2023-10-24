910b8a183813fa37cdccde32ad1a6deefaecd9f8
J.Seremak <janko2002@outlook.com>
2023-10-24 16:19:10 +0200

    Another API Release test

--- RestfulWebService/Program.cs
+++ RestfulWebService/Program.cs
@@ -10,7 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-
+ 
 builder.Services
     .AddScoped<ICanteenRepository, CanteenRepository>()
     .AddScoped<IPacketRepository, PacketRepository>()
