f75a39871230bab6c1cf368e8716918a54838bd8
J.Seremak <janko2002@outlook.com>
2023-10-27 15:44:58 +0200

    Commented

--- UI/Program.cs
+++ UI/Program.cs
@@ -99,14 +99,14 @@ builder.Services.AddAuthorization(policyBuilder => {
 var app = builder.Build();
 
 
-//// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline.
 //if (!app.Environment.IsDevelopment()) {
 //    app.UseExceptionHandler("/Home/Error");
 //    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 //    app.UseHsts();
 //}
 
-app.UseHsts();
+
 
 app.UseDeveloperExceptionPage();
 app.UseDatabaseErrorPage();
